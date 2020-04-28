9c91ac79de2b55cbbe57aaa3247ce58faa5f8281
udistr <udistr@gmail.com>
2020-04-28 18:44:31 -0400

    Add example figure

--- examples/Diffusion_1D.jl
+++ examples/Diffusion_1D.jl
@@ -14,19 +14,29 @@ rho0=exp.(-(x).^2)
 rho0=rho0/sum(rho0);
 DT=0.1;
 DX=1;
-NT=100;
+NT=120;
 rho1=zeros(length(rho0));
 
 crho0=CircularArray(rho0,grid);
 crho1=CircularArray(rho0*0,grid);
+plot_array = Any[]
 for i=1:NT
     crho1=crho0+(shiftc(crho0,dims=1,shift=1) + shiftc(crho0,dims=1,shift=-1) - (2 * crho0))*DT/DX^2
     println(i," = ",(maximum(crho1)))
     global crho0=crho1;
     if rem(i,10)==0;#3600*6/dt
-        plt=plot(x,crho1.data,ylims=(0,0.3))
+        plt=plot(x,crho1.data,ylims=(0,0.3),
+                              title=string(i/10," seconds"))
+        push!(plot_array,plt)
         println(mean(crho1))
-        display(plt)
-        sleep(1)
     end
 end
+plt=plot(plot_array...,legend=false,
+                   titlefontsize=6,
+                   ylabel=L"\rho~~[kg~m^{-3}]",
+                   xlabel="x [m]",
+                   xtickfontsize=6,
+                   xguidefontsize=6,
+                   ytickfontsize=6,
+                   yguidefontsize=6, dpi=200)
+png(plt,"Diffusion_1d.png")
