3019d724f32ac8f4dbc92dc33d9b4252855f2d2a
udistr <udistr@gmail.com>
2020-07-24 00:33:05 -0400

    Update advection example

--- examples/Advection_1D.jl
+++ examples/Advection_1D.jl
@@ -1,6 +1,7 @@
 using CyclicArrays
 using Plots
 using Statistics
+using LaTeXStrings
 
 # 1d array example
 
@@ -24,39 +25,21 @@ cu0=CyclicArray(u0,grid);
 cu1=CyclicArray(u0*0,grid);
 plot_array1 = Any[]
 plot_array2 = Any[]
-for i=1:NT
+anim = @animate for i ∈ 1:NT
+    println(i)
     crho1 = crho0 - crho0 * cu0 * DT/DX + shiftc(crho0 * cu0,dims=1,shift=-1) * DT/DX
     cu1 = (crho0 * cu0 - ( crho0 * cu0^2 -
                           shiftc(crho0 * cu0^2,dims=1,shift=-1) ) * DT / DX) / crho1
-    #println(i," = ",(maximum(cu1)))
     global cu0=cu1;
     global crho0=crho1;
-    if rem(i,200)==0;
         plt1=plot(x,crho1.data,ylims=(0,20),
-                              title=string(i/10," seconds"))
+                            ylabel=L"\rho~~[kg~m^{-3}]",
+                            xlabel="x [m]",
+                            xlim=(-1,1))
         plt2=plot(x,cu1.data,ylims=(0,1.2),
-                              title=string(i/10," seconds"))
-        push!(plot_array1,plt1)
-        push!(plot_array2,plt2)
-        #println(mean(crho1 * cu1)) - conservation of momentum
-        #println(mean(cu1))
-    end
-end
-plt=plot(plot_array1...,legend=false,
-                   titlefontsize=6,
-                   ylabel=L"\rho~~[kg~m^{-3}]",
-                   xlabel="x [m]",
-                   xtickfontsize=6,
-                   xguidefontsize=6,
-                   ytickfontsize=6,
-                   yguidefontsize=6, dpi=200)
-png(plt,"Advection_1D_rho.png")
-plt=plot(plot_array2...,legend=false,
-                   titlefontsize=6,
-                   ylabel=L"u~~[m/s]",
-                   xlabel="x [m]",
-                   xtickfontsize=6,
-                   xguidefontsize=6,
-                   ytickfontsize=6,
-                   yguidefontsize=6, dpi=200)
-png(plt,"Advection_1D_u.png")
+                            ylabel=L"u~~[m/s]",
+                            xlabel="x [m]",
+                            xlim=(-1,1))
+        plot(plt1,plt2,legend=false,title=string((i-1)/10," seconds"))
+end every 10
+gif(anim, "Advection1D.gif", fps = 30)
