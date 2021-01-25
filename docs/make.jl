f11e5a2c7c84862d8f028609407a754ea74376fe
Ehud Strobach <udistr@gmail.com>
2021-01-25 11:06:33 +0200

    Fix broken line

--- docs/make.jl
+++ docs/make.jl
@@ -8,8 +8,7 @@ makedocs(
     sitename = "CyclicArrays",
     format = Documenter.HTML(),
     modules=[CyclicArrays],
-    repo="julia --color=yes make.jl
-    ",
+    repo="julia --color=yes make.jl",
     authors="udistr <strobach.ehud@gmail.com>",
     pages = [
     "Home" => "index.md",
